15a84b066a202a8e32bdcee22d8334d80c24a4ca
ahmetveburak <ahmetbozyurtt@gmail.com>
2023-01-25 23:33:38 +0300

    fix: consecutive loops and typing

--- randimage/masks.py
+++ randimage/masks.py
@@ -1,18 +1,19 @@
 import random
+from typing import Tuple
+
 import numpy as np
 from scipy.stats import multivariate_normal
 from scipy.ndimage import gaussian_filter
 
 
 class BaseMask(object):
-    def __init__(self, shape) -> None:
+    def __init__(self, shape: Tuple[int, int]) -> None:
         self.shape = shape
 
 
 class SaltPepperMask(BaseMask):
     def get_mask(self):
-        self.mask = np.random.randint(0, 2, size=self.shape)
-        return self.mask
+        return np.random.randint(0, 2, size=self.shape)
 
 
 class NormalMask(BaseMask):
@@ -22,36 +23,35 @@ class NormalMask(BaseMask):
 
 
 class GaussianBlobMask(BaseMask):
-
     def get_mask(self, ncenters=None, sigma=None):
-        self.mask = np.zeros(self.shape)
+        mask = np.zeros(self.shape)
         if ncenters is None:
-            ncenters = random.randint(1, int(0.5*np.sqrt(self.mask.size)))
+            ncenters = random.randint(1, int(0.5 * np.sqrt(mask.size)))
         if sigma is None:
-            sigma = random.randint(1, int(0.2*np.sqrt(self.mask.size)))
+            sigma = random.randint(1, int(0.2 * np.sqrt(mask.size)))
         for center in range(ncenters + 1):
             cx = random.randint(0, self.shape[0] - 1)
             cy = random.randint(0, self.shape[1] - 1)
-            self.mask[cx, cy] = 1
-        self.mask = gaussian_filter(self.mask, sigma, mode='nearest')
-        return self.mask
+            mask[cx, cy] = 1
+        return gaussian_filter(mask, sigma, mode="nearest")
 
     def _get_gaussian_bell(self, center, sigma):
-        return lambda point: multivariate_normal(center, sigma*np.eye(2)).pdf(point)
+        return lambda point: multivariate_normal(center, sigma * np.eye(2)).pdf(point)
 
-    def get_mask_slow(self, ncenters=None):
-        if ncenters is None:
-            ncenters = random.randint(1, int(0.5*np.sqrt(self.mask.size)))
-        self.mask = np.zeros(self.shape)
-        gaussians = []
-        for center in range(ncenters + 1):
-            cx = random.randint(0, self.shape[0])
-            cy = random.randint(0, self.shape[1])
-            sigma = random.randint(1, int(0.2*np.sqrt(self.mask.size)))
-            gaussians.append(self._get_gaussian_bell((cx, cy), sigma))
-        for idx, _ in np.ndenumerate(self.mask):
-            self.mask[idx] = sum([f(idx) for f in gaussians])
-        return self.mask
+    # TODO Commented due to no use
+    # def get_mask_slow(self, ncenters=None):
+    #     if ncenters is None:
+    #         ncenters = random.randint(1, int(0.5 * np.sqrt(self.mask.size)))
+    #     self.mask = np.zeros(self.shape)
+    #     gaussians = []
+    #     for center in range(ncenters + 1):
+    #         cx = random.randint(0, self.shape[0])
+    #         cy = random.randint(0, self.shape[1])
+    #         sigma = random.randint(1, int(0.2 * np.sqrt(self.mask.size)))
+    #         gaussians.append(self._get_gaussian_bell((cx, cy), sigma))
+    #     for idx, _ in np.ndenumerate(self.mask):
+    #         self.mask[idx] = sum([f(idx) for f in gaussians])
+    #     return self.mask
 
 
 MASKS = (SaltPepperMask, NormalMask, GaussianBlobMask)
